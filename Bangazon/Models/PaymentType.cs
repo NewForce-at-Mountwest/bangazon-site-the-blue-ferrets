22fffc94f914a4e3c3d4223d419a4746c7b72b98
Mandy <drgnfli6420@yahoo.com>
2020-09-21 09:26:47 -0400

    Users can select a product category when selling a product

--- Bangazon/Models/PaymentType.cs
+++ Bangazon/Models/PaymentType.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -6,30 +6,30 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bangazon.Models
 {
-  public class PaymentType
-  {
-    [Key]
-    public int PaymentTypeId { get; set; }
+    public class PaymentType
+    {
+        [Key]
+        public int PaymentTypeId { get; set; }
 
-    [Required]
-    [DataType(DataType.Date)]
-    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-    public DateTime DateCreated { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
 
-    [Required]
-    [StringLength(55)]
-    public string Description { get; set; }
+        [Required]
+        [StringLength(55)]
+        public string Description { get; set; }
 
-    [Required]
-    [StringLength(20)]
-    public string AccountNumber { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string AccountNumber { get; set; }
 
-    [Required]
-    public string UserId {get; set;}
+        [Required]
+        public string UserId { get; set; }
 
-    [Required]
-    public ApplicationUser User { get; set; }
+        [Required]
+        public ApplicationUser User { get; set; }
 
-    public ICollection<Order> Orders { get; set; }
-  }
+        public ICollection<Order> Orders { get; set; }
+    }
 }
